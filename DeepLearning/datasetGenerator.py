d968f1ff68f71a4bdf6b49e3f32de3bc19e265e6
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-02-06 00:18:32 +0530

    added new gesture

--- DeepLearning/datasetGenerator.py
+++ DeepLearning/datasetGenerator.py
@@ -6,32 +6,33 @@ m = int(input("Enter Class : 1, 2, 3 or 4(neg) : "))
 
 
 cap = cv2.VideoCapture(0)
-
+sub = 'c'
 i = 0
 j = 0
 while True:
     
+    
     ret, frame = cap.read()
 
-    frame = cv2.resize(frame, (480,480))
+    frame = cv2.resize(frame, (224,224))
     if m == 1:
         i += 1
-        filename = "invideo/0/"+str(i)+".jpg"
+        filename = "invideo/0/"+str(i)+sub+".jpg"
         cv2.imwrite(filename, frame)
 
     elif m == 2:
         i += 1
-        filename = "invideo/0/"+str(i)+".jpg"
+        filename = "invideo/1/"+str(i)+sub+".jpg"
         cv2.imwrite(filename, frame)
 
     elif m == 3:
         i += 1
-        filename = "invideo/0/"+str(i)+".jpg"
+        filename = "invideo/2/"+str(i)+sub+".jpg"
         cv2.imwrite(filename, frame)
 
     elif m == 4:
         i += 1
-        filename = "invideo/0/"+str(i)+".jpg"
+        filename = "invideo/3/"+str(i)+sub+".jpg"
         cv2.imwrite(filename, frame)
 
         
